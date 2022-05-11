6be3e0f497329a476063e52236edb51156499312
ysaad01 <ysaad2325@gmail.com>
2022-05-11 16:15:06 -0400

    added homepage and used handlebars to generate posts to the page

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection')
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
