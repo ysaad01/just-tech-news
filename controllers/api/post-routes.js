0eae198084aed40e59e8491235000286677c8328
ysaad01 <ysaad2325@gmail.com>
2022-05-13 20:19:29 -0400

    logged in user has ability to comment on a post; login and logout links will cond render based on the user's session

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -102,12 +102,18 @@ router.post("/", (req, res) => {
 
 router.put("/upvote", (req, res) => {
   // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote, Comment, User })
-    .then((updatedVoteData) => res.json(updatedVoteData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  // make sure the session exists first
+  if (req.session) {
+    Post.upvote(
+      { ...req.body, user_id: req.session.user_id },
+      { Vote, Comment, User }
+    )
+      .then((updatedVoteData) => res.json(updatedVoteData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 router.put("/:id", (req, res) => {
